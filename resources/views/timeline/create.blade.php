3a296ee7dee42134dab6c06d5ea943bc7d82fddf
bethojunior <hgomescostajr@gmail.com>
2020-12-04 16:27:18 -0300

    last commit

---
+++ resources/views/timeline/create.blade.php
@@ -0,0 +1,37 @@
+
+@extends('layouts.page')
+@section('title', 'Listagem de usuários ')
+@section('content_header')
+    {{--    <h1 class="m-0 text-dark">Ficha do(a) {{ $data[0]['user'][0]['name'] }}</h1>--}}
+@stop
+
+@section('content')
+    @include('includes.alerts')
+    <div class="row col-lg-12 col-sm-12">
+        @foreach($data as $content)
+            <div class="card col-sm-12 col-lg-12">
+                <p>
+                    Titulo : {{ $content->title }}
+                </p>
+                <p>
+                    Data : {{ Carbon\Carbon::parse($content->created_at)->format('d/m/Y - H:m:s')  }} hrs
+                </p>
+                <p>
+                    Razão do procedimento : {{ $content->reason }}
+                </p>
+                <p>
+                    Descrição : {{ $content->description }}
+                </p>
+                <p>
+                    Procedimento : {{ $content->procedure }}
+                </p>
+            </div>
+        @endforeach
+    </div>
+@stop
+
+@section('js')
+    <script src="{{ asset('js/controllers/UserController/UserController.js') }}"></script>
+    <script src="{{ asset('js/modules/user/list.js') }}"></script>
+@endsection
+
