8c1fae3f49bac5ad3ee2aeb95e9481b56275498a
bethojunior <hgomescostajr@gmail.com>
2020-12-05 12:06:15 -0300

    client side, ok

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -44,7 +44,7 @@ class HomeController extends Controller
                 );
         }
 
-        return view('home.patiente')
+        return view('patient.profile')
             ->with
             (
                 [
--- resources/views/patient/profile.blade.php
+++ resources/views/patient/profile.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.page')
 @section('title', 'Listagem de usuários ')
 @section('content_header')
-    <h1 class="m-0 text-dark">Meus dados</h1>
+    <h1 class="m-0 text-dark">Meus Perfil</h1>
 @stop
 
 @section('content')
