fb71a1d90c5673719a535bdb0fbff8f2e8d04971
bethojunior <hgomescostajr@gmail.com>
2020-12-14 23:34:08 -0300

    addd name patient

--- resources/views/timeline/timeline.blade.php
+++ resources/views/timeline/timeline.blade.php
@@ -2,7 +2,7 @@
 @extends('layouts.page')
 @section('title', 'Listagem de usuários ')
 @section('content_header')
-{{--    <h1 class="m-0 text-dark">Ficha do(a) {{ $data[0]['user'][0]['name'] }}</h1>--}}
+    <h1 class="m-0 text-dark">Ficha do(a) paciente {{ $data[0]['user'][0]['name'] }}</h1>
 @stop
 
 @section('content')
@@ -10,14 +10,14 @@
     <div class="row col-lg-12 col-sm-12">
         @foreach($data as $content)
             <div class="card col-sm-12 col-lg-12 pt-2">
+{{--                <p>--}}
+{{--                    Paciente : {{ $content->user[0]['name'] }}--}}
+{{--                </p>--}}
                 <p>
-                    Paciente : {{ $content->user[0]['name'] }}
+                    Data : {{ Carbon\Carbon::parse($content->created_at)->format('d/m/Y - H:m:s')  }} hrs
                 </p>
                 <p>
                     Titulo : {{ $content->title }}
-                </p>
-                <p>
-                    Data : {{ Carbon\Carbon::parse($content->created_at)->format('d/m/Y - H:m:s')  }} hrs
                 </p>
                 <p>
                     Razão do procedimento : {{ $content->reason }}
